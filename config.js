122f215361d960947b59b944e13ef025fd262bee
M Usman <129891430+smanmani@users.noreply.github.com>
2023-07-31 00:11:25 +0500

    Update config.js

--- config.js
+++ config.js
@@ -11,17 +11,17 @@ global.github = 'https://github.com/prabathLK/PRABATH-MD'
 global.location = 'Sultanpur IN'
 global.gurl = 'https://instagram.com/' // add your username
 global.devs = '94762280384';
-global.website = 'https://github.com/prabathLK/PRABATH-MD' //wa.me/+94762280384
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/H062oEI.jpeg'
+global.website = 'https://github.com/prabathLK/PRABATH-MD' //wa.me/+923090658722
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/UCxG4Ok.jpeg'
 module.exports = {
   OPENAI_API_KEY:process.env.OPENAI_API_KEY || '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',
-  botname: process.env.BOT_NAME || 'PRABATH-MD',
+  botname: process.env.BOT_NAME || '𝑈𝑆𝛭𝛥𝛮-𝐵𝛩𝑇',
   dl_limit: process.env.DL_SIZE || '300',
   art: process.env.AUTO_RESTART || 'true',
-  sudo: process.env.SUDO || '94762280384',
-  alivepic: process.env.ALIVE_IMG || 'https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg',
-  alivemsj: process.env.ALIVE_MSJ || '*POWERD BY PRABATH-MD* 🎊',
-  ownername:process.env.OWNER_NAME || 'PRABATH',
+  sudo: process.env.SUDO || '923090658722',
+  alivepic: process.env.ALIVE_IMG || 'https://i.imgur.com/UCxG4Ok.jpeg',
+  alivemsj: process.env.ALIVE_MSJ || '*POWERD BY 𝑈𝑆𝛭𝛥𝛮-𝐵𝛩𝑇* 🎊',
+  ownername:process.env.OWNER_NAME || '𝑈𝑆𝛭𝛥𝛮',
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
   author: process.env.PACK_INFO.split(";")[0] || 'author', 
   auto_read_status : process.env.AUTO_READ_STATUS || 'false',
