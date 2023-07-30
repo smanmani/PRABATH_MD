cfe707243eebc00b068ab365020a1e0ec0f2590b
M Usman <129891430+smanmani@users.noreply.github.com>
2023-07-31 00:12:46 +0500

    Update config.js
    𝑈𝑆𝛭𝛥𝛮-𝐵𝛩𝑇

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ global.location = 'Sultanpur IN'
 global.gurl = 'https://instagram.com/' // add your username
 global.devs = '94762280384';
 global.website = 'https://github.com/prabathLK/PRABATH-MD' //wa.me/+94762280384
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/H062oEI.jpeg'
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/UCxG4Ok.jpeg'
 module.exports = {
   OPENAI_API_KEY:process.env.OPENAI_API_KEY || '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',
-  botname: process.env.BOT_NAME || 'PRABATH-MD',
+  botname: process.env.BOT_NAME || '𝑈𝑆𝛭𝛥𝛮-𝐵𝛩𝑇',
   dl_limit: process.env.DL_SIZE || '300',
   art: process.env.AUTO_RESTART || 'true',
   sudo: process.env.SUDO || '94762280384',
